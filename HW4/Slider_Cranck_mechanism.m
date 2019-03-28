e8eb9b90f507111e432dacc5abeb7d4f7365c91a
Mattia Cipriani <you@example.com>
2019-03-28 21:32:36 +0200

    last check

--- HW4/Slider_Cranck_mechanism.m
+++ HW4/Slider_Cranck_mechanism.m
@@ -23,12 +23,13 @@ d_values = zeros(1,N/0.1+1);
 
 % itializations of vectors for storing values of teta-dot and d-dot
 angular_velocity = zeros(1,N/0.1+1);
-tang_velocity = zeros(1,N/0.1+1);
+slider_velocity = zeros(1,N/0.1+1);
 
 % itializations of vectors for storing values of teta-dot-dot and d-dot-dot
 angular_acc = zeros(1,N/0.1+1);
-tang_acc = zeros(1,N/0.1+1);
+slider_acc = zeros(1,N/0.1+1);
 
+% initialization of a vector for storing values of fi
 crank_angle = zeros(1,N/0.1+1);
 
 % in the following lines the loop is run in order to implement the
@@ -60,66 +61,67 @@ for i = 1:N/0.1+1
     velocity = J2\ft;
     % storing values for teta-dot and d-dot
     angular_velocity(i) = velocity(1);
-    tang_velocity(i) = velocity(2);
+    slider_velocity(i) = velocity(2);
     
     % computation of the acceleration
     G = [a*w^2*cos(fi)+b*(angular_velocity(i))^2*cos(teta_values(i)); +a*w^2*sin(fi)-b*(angular_velocity(i))^2*sin(teta_values(i))];
     acc = J2\G;
     % storing values for teta-dot-dot and d-dot-dot
     angular_acc(i) = acc(1);
-    tang_acc(i) = acc(2);
+    slider_acc(i) = acc(2);
     
     
     
 end
 
-% plot teta in function of time
+% plot teta and d in function of time
 figure
 plot(t, teta_values);
-xlabel('t');
-ylabel('teta');
-title(sprintf('Teta(t) in an intervals of seconds from 0 to %d', N));
+xlabel('t [s]');
+ylabel('teta [rad]');
+title(sprintf('Teta(t)in an intervals of seconds from 0 to %d', N));
 
 % plot d in function of time
 figure
 plot(t, d_values);
-xlabel('t');
-ylabel('d = position');
+xlabel('t [s]');
+ylabel('d = position [m]');
 title(sprintf('d(t) in an intervals of seconds from 0 to %d', N));
 
 % plot the angular velocity in function of time
 figure
 plot(t,angular_velocity);
-xlabel('t');
-ylabel('angular velocity');
-title(sprintf('Angular velocity in an intervals of seconds from 0 to %d', N));
+xlabel('t [s]');
+ylabel('angular velocity [rad/s]');
+title(sprintf('angular velocity in an intervals of seconds from 0 to %d', N));
 
-% plot the tangential velocity in function of time
+% plot the slider velocity in function of time
 figure
-plot(t,tang_velocity);
-xlabel('t');
-ylabel('tangential velocity');
-title(sprintf('tangential velocity in an intervals of seconds from 0 to %d', N));
+plot(t,slider_velocity);
+xlabel('t [s]');
+ylabel('slider velocity [m/s]');
+title(sprintf('slider velocity in an intervals of seconds from 0 to %d', N));
 
 % plot the angular acceleration in function of time 
 figure
 plot(t,angular_acc);
-xlabel('t');
-ylabel('angular acc');
-title(sprintf('Angular acceleration in an intervals of seconds from 0 to %d', N));
+xlabel('t [s]');
+ylabel('angular acc [rad/s^2]');
+title(sprintf('angular acceleration in an intervals of seconds from 0 to %d', N));
 
-% plot the tangential acceleration
+% plot the slider acceleration
 figure
-plot(t,tang_acc);
-xlabel('t');
-ylabel('tangential acc');
-title(sprintf('tangential acceleration in an intervals of seconds from 0 to %d', N));
+plot(t,slider_acc);
+xlabel('t [s]');
+ylabel('slider acc [rad/s^2]');
+title(sprintf('slider acceleration in an intervals of seconds from 0 to %d', N));
 
-% plot the tangential acceleration in function of the crank angle
-figure
-plot(crank_angle,tang_acc);
-xlabel('fi = crank angle');
-ylabel('tangential acc');
-title(sprintf('tangential acceleration in function of fi', N));
-axis([0.5236 20.5236 -0.15 0.1]);
+% % plot the crank and slider acceleration and velocity in function of the crank angle
+% figure
+% plot(crank_angle,slider_acc,crank_angle, slider_velocity);
+% xlabel('fi = crank angle');
+% ylabel('slider acc');
+% title(sprintf('slider acceleration in function of fi', N));
+% axis([0.5236 20.5236 -0.2 0.2]);
+
 
